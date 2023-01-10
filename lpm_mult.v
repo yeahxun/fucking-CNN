1116c880d41b91a9ff7e58805b6f02fdfe4ceab7
yeahxun <112159078+yeahxun@users.noreply.github.com>
2023-01-10 15:36:05 +0900

    Update lpm_mult.v

--- lpm_mult.v
+++ lpm_mult.v
@@ -1,3 +1,4 @@
+
 module lpm_mult ( result, dataa, datab, sum, clock, clken, aclr );
 parameter lpm_type = "lpm_mult";
 parameter lpm_widtha = 1;
@@ -14,4 +15,16 @@ input  [lpm_widtha-1:0] dataa;
 input  [lpm_widthb-1:0] datab;
 input  [lpm_widths-1:0] sum;
 output [lpm_widthp-1:0] result;
+
+reg [lpm_widthp-1:0] result;
+
+
+always@(posedge clock)
+begin
+    if(aclr !=0)
+        result <= 0;
+    else 
+        result <= dataa * datab + sum;
+end        
+        
 endmodule
