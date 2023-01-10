e5d8cea9f3a69320357aa6ea8c423f376ef39d1e
yeahxun <112159078+yeahxun@users.noreply.github.com>
2023-01-10 22:36:05 +0900

    Update tb_dsp_multiplier.v

--- testbench/tb_dsp_multiplier.v
+++ testbench/tb_dsp_multiplier.v
@@ -1,3 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/01/10 21:00:00
+// Design Name: 
+// Module Name: tb_dsp_multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 module dsp_multiplier_tb();
     reg signed [7:0] dataa, datab;
     reg signed [7:0] sumin;
@@ -15,20 +35,24 @@ module dsp_multiplier_tb();
         .clken(clken),
         .aclr(aclr)
     );
-
+    always
+    begin
+        #5;
+        clock= ~clock;
+    end
+    
     initial begin
         // Stimulus goes here
+        clock=0;
         dataa = 8'h01;
         datab = 8'h02;
         sumin = 8'h03;
         clken = 1'b1;
         aclr = 1'b0;
-        #10 clock = 1'b1;
-        #10 clock = 1'b0;
-        #10;
+        #100;
+        dataa = 0;
+        datab = 1;
+        sumin = 8'h01;
     end
-
-    initial
-    #100 $finish;
 
 endmodule
