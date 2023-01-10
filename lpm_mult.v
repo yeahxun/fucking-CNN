a832a4c414caffd3d4e9265391b096c7e00840a5
yeahxun <112159078+yeahxun@users.noreply.github.com>
2023-01-10 15:20:15 +0900

    Create lpm_mult.v

---
+++ lpm_mult.v
@@ -0,0 +1,17 @@
+module lpm_mult ( result, dataa, datab, sum, clock, clken, aclr )
+parameter lpm_type = "lpm_mult";
+parameter lpm_widtha = 1;
+parameter lpm_widthb = 1;
+parameter lpm_widths = 1;
+parameter lpm_widthp = 1;
+parameter lpm_representation  = "UNSIGNED";
+parameter lpm_pipeline  = 0;
+parameter lpm_hint = "UNUSED";
+input  clock;
+input  clken;
+input  aclr;
+input  [lpm_widtha-1:0] dataa;
+input  [lpm_widthb-1:0] datab;
+input  [lpm_widths-1:0] sum;
+output [lpm_widthp-1:0] result;
+endmodule
